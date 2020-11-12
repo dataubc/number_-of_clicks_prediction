34b8019f4903030c2a9ef31c449c49e75fcb3aa2
Salama <dataubc@gmail.com>
2020-11-12 13:29:21 -0700

    adding docs to scripts

--- clean.py
+++ clean.py
@@ -1,17 +1,15 @@
 import pandas as pd
-import numpy as np
 import requests
 from io import StringIO
-import matplotlib.pyplot as plt
-import time
-from pandas.plotting import scatter_matrix
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.pipeline import Pipeline
-import sys
 
-def main(hotel_url ,output_path, type = 'train' ):
-    
-  
+
+def main(hotel_url, output_path, type='train'):
+    """
+    :param hotel_url: link for the data to be downloaded from
+    :param output_path: relative path where the cleaned data will be saved
+    :param type: train if the data to be cleaned contains the target variable, test if the data is test data set
+    :return:None: will write the cleaned data to the given out_put path
+    """
 
     def loading_data(file_url):
         """
@@ -26,39 +24,42 @@ def main(hotel_url ,output_path, type = 'train' ):
         """
 
         file_id = file_url.split('/')[- 2]
-        dwn_url='https://drive.google.com/uc?export=download&id=' + file_id
+        dwn_url = 'https://drive.google.com/uc?export=download&id=' + file_id
         url = requests.get(dwn_url).text
         csv_raw = StringIO(url)
         df = pd.read_csv(csv_raw)
         return df
- 
+
+    # reading the data
+    print('Reading data')
     hotel_data = loading_data(hotel_url)
+    # cleaning data
+    print('Cleaning data')
     hotel_data = hotel_data[hotel_data['content_score'].notnull()]
     hotel_data = hotel_data[hotel_data['n_images'] >= 0]
     hotel_data = hotel_data[hotel_data['distance_to_center'].notnull()]
     hotel_data['avg_rating'] = hotel_data['avg_rating'].fillna(0)
     hotel_data = hotel_data[hotel_data['stars'].notnull()]
     hotel_data = hotel_data[hotel_data['avg_price'].notnull()]
-    
     city_id_count = hotel_data['city_id'].value_counts().reset_index(name='count').rename(columns={'index': 'city_id'})
-    hotel_data = pd.merge(hotel_data,city_id_count ,on=['city_id'], how='left')
-    hotel_data = hotel_data.drop(columns = ['city_id'])
-    hotel_data['avg_saving_cash'] =hotel_data['avg_price']*hotel_data['avg_saving_percent']
-    
-    if type =='train':
-    
+    hotel_data = pd.merge(hotel_data, city_id_count, on=['city_id'], how='left')
+    hotel_data = hotel_data.drop(columns=['city_id'])
+    print('Creating new features')
+    hotel_data['avg_saving_cash'] = hotel_data['avg_price'] * hotel_data['avg_saving_percent']
+
+    # apply cleaning for target variable only if this is training data
+    if type == 'train':
         y = hotel_data['n_clicks']
         removed_outliers = y.between(y.quantile(.05), y.quantile(.95))
-        index_names = hotel_data[~removed_outliers].index 
+        index_names = hotel_data[~removed_outliers].index
         hotel_data.drop(index_names, inplace=True)
-        
         hotel_data['n_clicks'] = hotel_data['n_clicks'].astype(float)
-        
+    print('Saving cleaned data to ' + str(output_path))
     hotel_data['count'] = hotel_data['count'].astype(float)
-    hotel_data.to_csv(output_path,index =False)
-                                                           
+    hotel_data.to_csv(output_path, index=False)
+
 
 if __name__ == "__main__":
     import sys
-    
-    main(sys.argv[1] ,sys.argv[2], sys.argv[3])
+
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
--- xgboost_model.py
+++ xgboost_model.py
@@ -1,55 +1,23 @@
 import pandas as pd
 import numpy as np
-import requests
-from io import StringIO
-
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier, AdaBoostRegressor,  BaggingRegressor, ExtraTreesRegressor, GradientBoostingRegressor, RandomForestRegressor
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, MaxAbsScaler,MinMaxScaler,PowerTransformer
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.metrics import mean_squared_error
-from sklearn.base import clone, TransformerMixin
-from sklearn.model_selection import KFold
-import time
-import pickle
-from pandas.plotting import scatter_matrix
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import RidgeCV
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import SGDRegressor
 import xgboost
-from xgboost import XGBRegressor
-from sklearn.model_selection import RepeatedStratifiedKFold
 
-
+# reading data
 hotel_data = pd.read_csv('cleaned_train.csv')
-X = hotel_data.drop(columns = ['n_clicks','hotel_id'])
-# let's also add the new featture avg_saving_cash
-X['avg_saving_cash'] =X['avg_price']*X['avg_saving_percent']
+X = hotel_data.drop(columns=['n_clicks', 'hotel_id'])
+# let's also add the new feature avg_saving_cash
+X['avg_saving_cash'] = X['avg_price'] * X['avg_saving_percent']
 y = hotel_data['n_clicks']
 
-    
-
+# let's create trained data for xgboost
 dtrain = xgboost.DMatrix(X, label=y)
 
-params = {
-    # These are the hyper-parameters that we are going to tune.
-    'max_depth':6,
-    'min_child_weight': 3,
-    'eta':.1,
-    'subsample': 1,
-    'colsample_bytree': 0.7,
-    # Other parameters
-    'objective':'reg:squarederror',
-}
-params['eval_metric'] = "rmse"
+params = {'max_depth': 6, 'min_child_weight': 3, 'eta': .1, 'subsample': 1, 'colsample_bytree': 0.7,
+          'objective': 'reg:squarederror', 'eval_metric': "rmse"}
 num_boost_round = 999
 print('Training phase has started')
 
+# training best model on the optimized hyper-parameters.
 best_model = xgboost.train(
     params,
     dtrain,
@@ -58,18 +26,15 @@ best_model = xgboost.train(
 print('Saving the model as best_model.model')
 best_model.save_model("best_model.model")
 print('Reading test data')
+
+# reading test data
 X_test = pd.read_csv('cleaned_test.csv')
-dtest = xgboost.DMatrix(X_test.drop(columns = ['hotel_id']))
+dtest = xgboost.DMatrix(X_test.drop(columns=['hotel_id']))
 predicted_y = best_model.predict(dtest)
 X_test['n_clicks'] = predicted_y
 # getting all negative prediction to 0
-X_test['n_clicks'] = np.where(X_test['n_clicks'] < 0,0,X_test['n_clicks'])
-final_result = X_test[['hotel_id','n_clicks']]
+X_test['n_clicks'] = np.where(X_test['n_clicks'] < 0, 0, X_test['n_clicks'])
+final_result = X_test[['hotel_id', 'n_clicks']]
 print('Saving the prediction as predictions.csv')
+# saving the result
 final_result.to_csv('predictions.csv')
-
-
-
-
-
-
